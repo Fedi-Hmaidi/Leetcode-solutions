8b87fa1a75ca41a0b654c7f982130d5085145a55
Fedi-Hmaidi <121828581+Fedi-Hmaidi@users.noreply.github.com>
2024-01-21 12:46:45 +0100

    Update 0014-longest-common-prefix.cpp

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -17,3 +17,32 @@ public:
         return res;
     }
 };
+
+//other solution
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string s = strs[0];
+        string res = "";
+
+        for(int i=0; i<strs.size(); i++){ //find minimum string length: it could be he max prefix
+            if(strs[i].size() < s.size()){
+                s = strs[i];
+            }
+        }
+        int index = 0;
+        while(index < s.size()){
+            char ch = s[index];
+            for(int i=0; i<strs.size(); i++){
+                if(ch == strs[i][index]){
+                   continue;
+                }
+                return s.substr(0,index);
+            }
+            index++;
+            res+=ch;
+        }
+        return res;
+    }
+};
