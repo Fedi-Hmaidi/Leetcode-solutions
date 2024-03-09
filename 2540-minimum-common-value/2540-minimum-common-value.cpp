5ce507b078dee303d85b4842c5b8ef698d278df8
Fedi-Hmaidi <121828581+Fedi-Hmaidi@users.noreply.github.com>
2024-03-09 09:42:22 +0100

    Time: 154 ms (9.17%), Space: 69.1 MB (14.66%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,19 +1,17 @@
+
 class Solution {
 public:
-    int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int> mp;
-        map<int,int> mp2;
-        for(auto x:nums1){
+    int getCommon(std::vector<int>& nums1, std::vector<int>& nums2) {
+        map<int, int> mp;
+        for (auto x : nums1) {
             mp[x]++;
         }
-          for(auto x:nums2){
-            mp2[x]++;
-        }
-        for (auto x : mp) {
-            if (mp2.find(x.first) != mp2.end()) {
-                return x.first;
+        for (auto x : nums2) {
+            if (mp.find(x) != mp.end()) {
+                return x;
             }
-        }   
+        }
+
         return -1;
     }
 };
