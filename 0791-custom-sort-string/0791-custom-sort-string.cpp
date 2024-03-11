b6263b8db5675536626cbe462f2023ab6deb5398
Fedi-Hmaidi <121828581+Fedi-Hmaidi@users.noreply.github.com>
2024-03-11 10:00:23 +0100

    Time: 7 ms (8.56%), Space: 9.9 MB (6.35%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -3,11 +3,10 @@ public:
     string customSortString(string order, string s) {
        map<char, int> mp;
         //khabi l index mtaa char l mwjoudin f string order w baad sorti hasb order index f map 
-        for (auto x : s) {
-            if (order.find(x) != string::npos) {
-                mp[x] = order.find(x);
-            }
+        for (int i = 0; i < order.size(); i++) {
+            mp[order[i]] = i;
         }
+
         sort(s.begin(), s.end(),[&](char a, char b) {
             return mp[a] < mp[b];
         });
