47bdd7698c7010ff09f7ebb58b5b22a4d1b82331
Fedi-Hmaidi <121828581+Fedi-Hmaidi@users.noreply.github.com>
2024-04-15 23:07:34 +0100

    Time: 9 ms (69.29%), Space: 13.5 MB (47.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
+    
+    static bool compare(pair<int,int> p1, pair<int,int> p2){
+        return p1.second < p2.second;
+    }
+    
     vector<int> twoSum(vector<int>& v, int t) {
-        map<int,int> mp;
-        vector<int> res;
-        
-        for (int i=0; i<v.size(); i++){
-            if(mp.find(t-v[i]) != mp.end()){
-                res.push_back(i);
-                res.push_back(mp[t-v[i]]);
-                return res;
-            }
-            mp[v[i]]=i;
+        vector<pair<int,int>> vp;
+        for(int i=0; i<v.size(); i++){
+            vp.push_back({i,v[i]});
         }
-    return{};
+        sort(vp.begin(), vp.end(), compare);
+        
+        int i = 0; 
+        int j = v.size()-1;
+        vector<int> ans;
+        while(i < j){
+            if(vp[i].second + vp[j].second == t){
+                ans.push_back(vp[i].first);
+                ans.push_back(vp[j].first);
+                return ans;
+            }else if(vp[i].second + vp[j].second > t){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return {};
     }
 };
